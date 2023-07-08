df7ce1ad7ac1eb8d4b8c75d8ac5580a85c729b33
QUANDEVV <vercelploy@gmail.com>
2023-07-08 14:11:07 +0300

    Done

--- script.js
+++ script.js
@@ -20,42 +20,43 @@ fetch('db.json')
       typeSelect.appendChild(option);
     });
 
-    // Show alert for successful data fetching
-    alert('Pizza data loaded successfully!');
+    // Display selections in the table (moved this code inside the fetch callback)
+    document.getElementById('pizza-form').addEventListener('submit', function(e) {
+      e.preventDefault();
+
+      // Get user selections
+      var size = document.getElementById('size').value;
+      var type = document.getElementById('type').value;
+      var crustings = [];
+      var crustingsInputs = document.getElementsByName('crustings');
+      for (var i = 0; i < crustingsInputs.length; i++) {
+        if (crustingsInputs[i].checked) {
+          crustings.push(crustingsInputs[i].value);
+        }
+      }
+
+      // Calculate total cost
+      var crustingsCost = crustings.length * 50;
+      var totalCost = parseInt(size) + parseInt(type) + crustingsCost;
+
+      // Display selections in the table
+      document.getElementById('size-selection').textContent = document.getElementById('size').options[document.getElementById('size').selectedIndex].text;
+      document.getElementById('type-selection').textContent = document.getElementById('type').options[document.getElementById('type').selectedIndex].text;
+      document.getElementById('crustings-selection').textContent = crustings.join(', ');
+      document.getElementById('total-cost').textContent = `Ksh ${totalCost}`;
+
+      // Show delivery details
+      document.getElementById('delivery-details').style.display = 'block';
+
+      // Show alert for successful order placement
+      
+    });
+
+    // Show alert for successful data fetching (moved this alert inside the fetch callback)
+  
   })
   .catch(error => {
     console.log(error);
     // Show alert for error in data fetching
     alert('An error occurred while fetching pizza data.');
   });
-
-document.getElementById('pizza-form').addEventListener('submit', function(e) {
-  e.preventDefault();
-
-  // Get user selections
-  var size = document.getElementById('size').value;
-  var type = document.getElementById('type').value;
-  var crustings = [];
-  var crustingsInputs = document.getElementsByName('crustings');
-  for (var i = 0; i < crustingsInputs.length; i++) {
-    if (crustingsInputs[i].checked) {
-      crustings.push(crustingsInputs[i].value);
-    }
-  }
-
-  // Calculate total cost
-  var crustingsCost = crustings.length * 50;
-  var totalCost = parseInt(size) + parseInt(type) + crustingsCost;
-
-  // Display selections in the table
-  document.getElementById('size-selection').textContent = document.getElementById('size').options[document.getElementById('size').selectedIndex].text;
-  document.getElementById('type-selection').textContent = document.getElementById('type').options[document.getElementById('type').selectedIndex].text;
-  document.getElementById('crustings-selection').textContent = crustings.join(', ');
-  document.getElementById('total-cost').textContent = `Ksh ${totalCost}`;
-
-  // Show delivery details
-  document.getElementById('delivery-details').style.display = 'block';
-
-  // Show alert for successful order placement
-  alert('Order placed successfully!');
-});
