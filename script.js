5cb40a25f8395c8963099b92962f8c0aa412184b
QUANDEVV <vercelploy@gmail.com>
2023-07-08 15:18:15 +0300

    added

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 document.addEventListener('DOMContentLoaded', function() {
-  // Show delivery details
   document.getElementById('delivery-details').style.display = 'block';
 
   fetch('db.json')
@@ -23,41 +22,66 @@ document.addEventListener('DOMContentLoaded', function() {
         option.textContent = `${type.name} [$${type.price}]`;
         typeSelect.appendChild(option);
       });
-
-      // Show alert for successful data fetching
-      alert('Pizza data loaded successfully!');
     })
     .catch(error => {
       console.log(error);
       // Show alert for error in data fetching
       alert('An error occurred while fetching pizza data.');
     });
+});
 
-  document.getElementById('pizza-form').addEventListener('submit', function(e) {
-    e.preventDefault();
 
-    // Get user selections
-    var size = document.getElementById('size').value;
-    var type = document.getElementById('type').value;
-    var crustings = [];
-    var crustingsInputs = document.getElementsByName('crustings');
-    for (var i = 0; i < crustingsInputs.length; i++) {
-      if (crustingsInputs[i].checked) {
-        crustings.push(crustingsInputs[i].value);
-      }
+document.getElementById("pizza-form").addEventListener("submit", function(event) {
+  event.preventDefault(); // Prevent form submission
+
+  // Get selected values
+  var size = document.getElementById("size").value;
+  var type = document.getElementById("type").value;
+  var crustings = [];
+  var crustingsInputs = document.getElementsByName("crustings");
+  for (var i = 0; i < crustingsInputs.length; i++) {
+    if (crustingsInputs[i].checked) {
+      crustings.push(crustingsInputs[i].value);
     }
+  }
 
-    // Calculate total cost
-    var crustingsCost = crustings.length * 50;
-    var totalCost = parseInt(size) + parseInt(type) + crustingsCost;
+  // Calculate total cost
+  var crustingsCost = crustings.length * 50;
+  var totalCost = parseInt(size) + parseInt(type) + crustingsCost;
 
-    // Display selections in the table
-    document.getElementById('size-selection').textContent = document.getElementById('size').options[document.getElementById('size').selectedIndex].text;
-    document.getElementById('type-selection').textContent = document.getElementById('type').options[document.getElementById('type').selectedIndex].text;
-    document.getElementById('crustings-selection').textContent = crustings.join(', ');
-    document.getElementById('total-cost').textContent = `Ksh ${totalCost}`;
+  // Display selections in the table
+  document.getElementById('size-selection').textContent = document.getElementById('size').options[document.getElementById('size').selectedIndex].text;
+  document.getElementById('type-selection').textContent = document.getElementById('type').options[document.getElementById('type').selectedIndex].text;
+  document.getElementById('crustings-selection').textContent = crustings.join(', ');
+  document.getElementById('total-cost').textContent = `Ksh ${totalCost}`;
+});
 
-    // Show delivery details
-    document.getElementById('delivery-details').style.display = 'block';
-  });
+document.getElementById("submit-button").addEventListener("click", function(event) {
+  event.preventDefault(); // Prevent form submission
+
+  // Get selected values
+  var size = document.getElementById("size").value;
+  var type = document.getElementById("type").value;
+  var crustings = [];
+  var crustingsInputs = document.getElementsByName("crustings");
+  for (var i = 0; i < crustingsInputs.length; i++) {
+    if (crustingsInputs[i].checked) {
+      crustings.push(crustingsInputs[i].value);
+    }
+  }
+
+  // Get location details
+  var estate = document.getElementById("estate").value;
+  var phoneNumber = document.getElementById("phone-number").value;
+
+  // Display alert with order details
+  var message = "SUCCESSFULY BOOKED:\n";
+  message += "Size: " + size + "\n";
+  message += "Type: " + type + "\n";
+  message += "Crustings: " + crustings.join(", ") + "\n";
+  message += "ESTATE: " + estate + "\n";
+  message += "Phone Number: " + phoneNumber;
+ 
+
+  alert(message);
 });
